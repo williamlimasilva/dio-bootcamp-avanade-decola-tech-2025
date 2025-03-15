eccf095b078b67df2ab12f98ba41c0b6893cc09c
williamlimasilva <williamtakuma@yahoo.com.br>
2025-03-15 08:10:19 -0300

    feat(api): initialize barber shop API structure with Gradle configuration and application properties

---
+++ desafios/Criando um APP Full Stack de Agendamento de Barbearia com Java e Angular/barber-shop-api/src/main/java/br/com/dio/barbershopapi/BarberShopApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.barbershopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberShopApiApplication.class, args);
+	}
+
+}
