5da89b458568acbce08c1dd36d1fdacaf7989ef1
williamlimasilva <williamtakuma@yahoo.com.br>
2025-03-18 10:39:19 -0300

    refactor(style): enhance UI components with improved styles and layout adjustments

--- desafios/Criando um APP Full Stack de Agendamento de Barbearia com Java e Angular/barber-shop-ui/src/app/services/dialog-manager.service.ts
+++ desafios/Criando um APP Full Stack de Agendamento de Barbearia com Java e Angular/barber-shop-ui/src/app/services/dialog-manager.service.ts
@@ -1,9 +1,23 @@
+import { ComponentType } from '@angular/cdk/portal';
 import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { YesNoDialogComponent } from '../commons/components/yes-no-dialog/yes-no-dialog.component';
+import { DialogManagerInterface } from './dialog-manager.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class DialogManagerService {
-
-  constructor() { }
+export class DialogManagerService implements DialogManagerInterface {
+  constructor(private readonly dialog: MatDialog) {}
+  showYesNoDialog(
+    component: ComponentType<YesNoDialogComponent>,
+    data: { title: string; content: string }
+  ): Observable<any> {
+    const dialogRef = this.dialog.open(component, {
+      width: '40rem',
+      data,
+    });
+    return dialogRef.afterClosed();
+  }
 }
