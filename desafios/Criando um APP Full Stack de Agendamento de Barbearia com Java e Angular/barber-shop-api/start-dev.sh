829a24ed027031834f960e242d9ac3758da4aa85
williamlimasilva <williamtakuma@yahoo.com.br>
2025-04-08 16:14:06 -0300

    feat(docker): add Docker support and PostgreSQL configuration for barber shop API

--- desafios/Criando um APP Full Stack de Agendamento de Barbearia com Java e Angular/barber-shop-api/start-dev.sh
+++ desafios/Criando um APP Full Stack de Agendamento de Barbearia com Java e Angular/barber-shop-api/start-dev.sh
@@ -1,2 +1,17 @@
-gradle clean
-gradle bootRun
+#!/bin/bash
+
+# Permissão de execução para o gradlew
+chmod +x ./gradlew
+
+echo "Aguardando a disponibilidade do PostgreSQL..."
+while ! pg_isready -h db -U barber-shop-api > /dev/null 2> /dev/null; do
+    echo "PostgreSQL indisponível - aguardando..."
+    sleep 2
+done
+echo "PostgreSQL disponível!"
+
+echo "Limpando o projeto..."
+./gradlew clean
+
+echo "Iniciando a aplicação..."
+./gradlew bootRun
